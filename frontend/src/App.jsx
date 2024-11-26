4320d9afb5b680161f1cfe7773c15824b1930e1e
soongxian <soongxian@gmail.com>
2024-11-26 23:35:38 +0800

    Added Header and Latest Collection in Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom'
-import About from './pages/About'
-import Cart from './pages/Cart'
-import Collection from './pages/Collection'
-import Contact from './pages/Contact'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Orders from './pages/Orders'
-import PlaceOrder from './pages/PlaceOrder'
-import Product from './pages/Product'
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
 import Navbar from "./components/Navbar";
-
 
 const App = () => {
   return (
     <div>
-      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/collection' element={<Collection/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/product/:productId' element={<Product/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/orders' element={<Orders/>}/>
-          <Route path='/place-order' element={<PlaceOrder/>}/>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
         </Routes>
       </div>
     </div>
