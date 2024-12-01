310eb02895e38fb255879b2642c66575c9b13930
RyanLIM_Laptop\RyanLIM <soongxian@gmail.com>
2024-12-01 11:01:22 +0800

    Add collapsible for filter in small screen

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/frontend_assets/assets";
 
 const Collection = () => {
   const { product } = useContext(ShopContext);
@@ -8,9 +9,14 @@ const Collection = () => {
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t ">
       {/* Filter Option */}
-      <div className="min-w-60">
+      <div onClick={()=>setShowFilter(!showFilter)} className="min-w-60">
         <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
           FILTERS
+          <img
+            className={`h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+            src={assets.dropdown_icon}
+            alt=""
+          />
         </p>
         {/* Category Filter */}
         <div
@@ -33,20 +39,23 @@ const Collection = () => {
         </div>
         {/* SubCategory Filter */}
         <div
-          className={`border border-gray-300 pl-5 py-3 mt-6 ${
+          className={`border border-gray-300 pl-5 py-3 mt-5 ${
             showFilter ? "" : "hidden"
           } sm:block`}
         >
           <p className="mb-3 text-sm font-medium">TYPE</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={"Topwear"} /> Topwear 
+              <input className="w-3" type="checkbox" value={"Topwear"} />{" "}
+              Topwear
             </p>
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={"Bottomwear"} /> Bottomwear
+              <input className="w-3" type="checkbox" value={"Bottomwear"} />{" "}
+              Bottomwear
             </p>
             <p className="flex gap-2">
-              <input className="w-3" type="checkbox" value={"Winterwear"} /> Winterwear
+              <input className="w-3" type="checkbox" value={"Winterwear"} />{" "}
+              Winterwear
             </p>
           </div>
         </div>
